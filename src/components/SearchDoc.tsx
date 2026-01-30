ecd1dad8ffda27e75fb8f579fdbefcece7b69f32
lr.cjbatuigas@mmdc.mcl.edu.ph <162258062+CjBatuigas@users.noreply.github.com>
2026-01-30 14:33:00 +0800

    Add doc and search doc updated

--- src/components/SearchDoc.tsx
+++ src/components/SearchDoc.tsx
@@ -18,39 +18,64 @@ const items = [
 const SearchDoc = () => {
   return (
     <div>
-        <div className="dropdown-center">
-            <label>Issuance Type:</label>
-            <button className="btn btn-secondary dropdown-toggle m-3 btn-IssuanceType" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                --SELECT--
-            </button>
-            <ul className="dropdown-menu">
-                {items.map(item => <li key={item}><a className="dropdown-item">{item}</a></li>)}
-            </ul>
-        </div>
+        <div className="container">
+        <h2 className="text-center">Search Documents</h2>
+        
 
-        <form className="row g-3">
-            <div className="col-md-4 d-inline-flex text-center">
-                <label htmlFor="inputInsuanceNo" className="form-label">Insuance No.</label>
-                <input type="number" className="form-control" id="inputInsuanceNo" />
+        <form className="d-flex flex-column align-items-center g-3">
+            <div className="row w-100 justify-content-center">
+                <div className="col-md-6 mb-3">
+                <label htmlFor="inputState" className="form-label">Issuance Type</label>
+                <select id="inputState" className="form-select text-center">
+                    {items.map(item => (
+                    <option key={item}>{item}</option>
+                    ))}
+                </select>
+                </div>
             </div>
-            <div className="col-md-4 d-inline-flex text-center">
-                <label htmlFor="inputSeries" className="form-label">Series</label>
-                <input type="text" className="form-control" id="inputSeries" />
+
+            <div className="row w-100 justify-content-center">
+                <div className="col-md-3 mb-3">
+                <label htmlFor="inssuaceNo" className="form-label">Issuance No.</label>
+                <input type="text" className="form-control" id="inssuaceNo" />
+                </div>
+
+                <div className="col-md-3 mb-3">
+                <label htmlFor="series" className="form-label">Series</label>
+                <input type="text" className="form-control" id="series" />
+                </div>
             </div>
-            <div className="col-md-6 d-inline-flex text-center">
-                <label htmlFor="inputSubject" className="form-label">Subject</label>
-                <input type="text" className="form-control" id="inputSubject" />
+
+            <div className="row justify-content-center w-100">
+                <div className="col-md-6 mb-3">
+                    <label htmlFor="date" className="form-label">Date</label>
+                    <input type="date" className="form-control" id="date" />
+                </div>
             </div>
-            <div className="col-md-6 d-inline-flex text-center">
-                <label htmlFor="inputSubject" className="form-label">Subject</label>
-                <input type="date" id="date" name="date" />
+
+
+            <div className="row w-100 justify-content-center">
+                <div className="col-md-6 mb-3">
+                    <label htmlFor="inputSubject" className="form-label">Subject</label>
+                    <input type="text" className="form-control" id="inputSubject" />
+                </div>
             </div>
-            <div className="col-md-6 d-inline-flex text-center">
-                <label htmlFor="inputKeyWords" className="form-label">Key Words</label>
-                <input type="text" className="form-control" id="inputKeyWords" />
+
+            <div className="row w-100 justify-content-center">
+                <div className="col-md-6 mb-3 ">
+                    <label htmlFor="inputKeyWords" className="form-label">Key Words</label>
+                    <input type="text" className="form-control" id="inputKeyWords"/>
+                </div>
             </div>
-            <button type="button" className="btn btn-primary">Search</button>
+            <div className="mb-3">
+            <button type="submit" className="btn btn-primary">Search</button>
+            </div>
+
+            
+
+
         </form>
+        </div>
     </div>
   );
 }
